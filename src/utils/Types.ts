65b308b212a0ce7bc1c1431c7109d0c3b12a8ad8
prathameshpatil455 <132359689+prathameshpatil455@users.noreply.github.com>
2024-03-27 09:26:21 +0530

    intial commit

---
+++ src/utils/Types.ts
@@ -0,0 +1,28 @@
+export interface AppTypeInitialState {}
+
+export interface PokemonTypeInitialState {
+    allPokemon: undefined | genericPokemonType[];
+    randomPokemons: undefined | generatedPokemonType[];
+}
+
+export interface genericPokemonType {
+    name: string;
+    url: string;
+}
+
+export interface generatedPokemonType {
+    name: string;
+    id: number;
+    image: string;
+    types: pokemonTypeInterface[]
+}
+
+export interface pokemonTypeInterface {
+    [key: string] : {
+        image: string;
+        resistance: string[];
+        strength: string[];
+        weakness: string[];
+        vulnerable: string[];
+    };
+}
